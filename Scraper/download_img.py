4e3470844694242d24403d47d0bbdbda8a1bb7cc
yoonseo <qnq0615@gmail.com>
2024-07-05 13:38:11 +0900

    Scraper menu 저장 기능 추가

--- Scraper/download_img.py
+++ Scraper/download_img.py
@@ -4,8 +4,13 @@ import os
 async def download_image(page, img_url, download_folder, file_name):
     os.makedirs(download_folder, exist_ok=True)
     img_path = os.path.join(download_folder, file_name)
-    async with page.expect_download() as download_info:
-        await page.evaluate(f"fetch('{img_url}').then(res => res.blob()).then(blob => {{ const a = document.createElement('a'); a.href = URL.createObjectURL(blob); a.download = '{file_name}'; document.body.appendChild(a); a.click(); document.body.removeChild(a); }});")
-    download = await download_info.value
-    await download.save_as(img_path)
-    print(f"Downloaded image to {img_path}")
+    
+    # Check if the image already exists
+    if not os.path.exists(img_path):
+        async with page.expect_download() as download_info:
+            await page.evaluate(f"fetch('{img_url}').then(res => res.blob()).then(blob => {{ const a = document.createElement('a'); a.href = URL.createObjectURL(blob); a.download = '{file_name}'; document.body.appendChild(a); a.click(); document.body.removeChild(a); }});")
+        download = await download_info.value
+        await download.save_as(img_path)
+        print(f"Downloaded image to {img_path}")
+    else:
+        print(f"Image already exists at {img_path}, skipping download.")
